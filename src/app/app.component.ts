279e5cb89c7f28e58d7e91be6bdae4c751084fba
yalinmuslem <yalin.muslem@gmail.com>
2018-03-16 13:05:17 +0700

    UPDATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, MenuController } from 'ionic-angular';
+import { Platform, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { DataMongodbPage } from '../pages/data-mongodb/data-mongodb'
+import { DataMongodbService } from '../pages/data-mongodb/data-mongodb.services'
 
 @Component({
   templateUrl: 'app.html',
@@ -12,15 +13,15 @@ import { DataMongodbPage } from '../pages/data-mongodb/data-mongodb'
 
 export class MyApp {
 	@ViewChild('mycontent') nav
-	
-	public menuCtrl: MenuController
 
 	rootPage:any = HomePage;
 	Menu: any[] = [
+		{ link: 'Beranda', pages: HomePage },
 		{ link: 'Data', pages: DataMongodbPage }
 	];
+	datalist: any = []
 
-	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public events: Events, private dataMongodb: DataMongodbService) {
 		platform.ready().then(() => {
 			// Okay, so the platform is ready and our plugins are available.
 			// Here you can do any higher level native things you might need.
